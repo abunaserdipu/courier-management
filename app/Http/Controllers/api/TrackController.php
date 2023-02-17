7ebbd9e32b37f22fcf63996fe6f1d4ec2dfa9851
abunaserdipu <abunaserdipu0@gmail.com>
2023-02-17 14:52:53 +0600

    query on join table for show track search result

--- app/Http/Controllers/api/TrackController.php
+++ app/Http/Controllers/api/TrackController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TrackController extends Controller
 {
     public function track(Request $req)
     {
-        $result = $req->all();
+        $search_value = $req->item;
+        $result = DB::table('orders')->where('sender_contact', $search_value)->join('order_statuses', 'orders.id', '=', 'order_statuses.id')->select('orders.*', 'order_statuses.status')->get();
         return $result;
     }
 }
